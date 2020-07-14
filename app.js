9192bacb1f4dae031cf32032fb0b083ca5170a41
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-07-13 22:43:23 -0400

    basic angular display data

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
   $scope.test = 'Hello world!';
+  $scope.posts = [
+    {title: 'post 1', upvotes: 3},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 1},
+  ]
 }]);
